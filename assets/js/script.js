33a182a97398298aa41a26ecb0afbed30658ec9d
Natalya Colburn <greatqueenbee@gmail.com>
2022-03-25 22:39:26 -0600

    changed the color change function wth tutor, doesn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,43 @@
-var hoursInday = {
-    morning: ["12AM", "01AM", "02AM","03AM", "04AM","05AM", "06AM","07AM", "08AM",],
-    business: ["09AM", "10AM", "11AM", "12PM", "01PM", "02PM","03PM","04PM","05PM","06PM", ],
-    night: ["07PM","08PM","09PM","10PM","11PM","12AM",]
+let timeNow = moment().format('dddd, MMMM Do YYYY');
+let hourNow = moment().format('h');
+let hourNow24= parseInt(moment().format('HH'));
+let hourNowInt = parseInt(hourNow);
+let Timer;
 
-};
 
-function displayTime() {
-    var rightNow = moment().format ('MMMM Do YYYY, h:mm:ss a')
-    timeDisplayEl.text(rightNow);
-}
+$('#currentDay').append(timeNow);
 
-setInterval(displayTime, 1000);
-colorChange();
+colorChanguing();
 
-function colorChange() {
-    presentTime = moment().format("hhA");
-    console.log(presentTime);
+function colorChanguing() {
 
-    if (hoursInday.morning.indexOf(presentTime)  {
-    night.indexOf(presentTime) !==-1; {
-        $(".hourNotes").css("background-color",)
-        console.log("Boom");
+  Timer = setInterval(colorChanguing, 1000);
+  
+  if(hourNow24 >= 9 && hourNow24 <= 17) {
 
+    for (let i =1; i<=9 ; i++) { 
+     let hourInInt = parseInt($('#time'+i).text());
+
+      if (hourInInt < 9) {
+        hourNow = hourInInt + 12;
+      }
+      
+      if (hourInInt == hourNow24) {
+        $('#text'+i).css('background-color', '#FB8F78');
+        continue;
+      }
+      
+      if (hourInInt < hourNow24) {
+        $('#text'+i).css('background-color', 'lightgray');
+      } 
+      else {
+        $('#text'+i).css('background-color', 'lightgreen');
+      }
     }
-    if (hoursInday.business.indexOf(presentTime)  {
-        $("#"+presentTime).css("background-color",)
-    }
-    for (var i=o; i<hoursInday.business.indexOf(presentTime)
-    
+
+  }
+  else {
+    clearInterval(Timer);
+    $('textarea').css('background-color', 'teal');
+  }
 }
